b82d428add757ffbec0a0660c7ebe8c356293c78
Duncan Hamill <duncanrhamill@googlemail.com>
2021-03-08 18:42:15 +0000

    Added toolchain configs for gsi-server

--- gsi-server/src/main.rs
+++ gsi-server/src/main.rs
@@ -1,3 +1,19 @@
+//! # Ground Service Interface Server
+//!
+//! The GSI server provides a web API to access the service interface of the OBC software.
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[cfg(target = "armv7-unknown-linux-gnueabihf")]
+use rppal;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
